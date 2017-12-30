7a6a094ea07ecee763e48d3a629a21f1bed58239
Torsten Link <torstenlink@gmx.ce>
2017-12-30 15:01:18 +0100

    WIP

--- tests/RunnerTest.js
+++ tests/RunnerTest.js
@@ -1,10 +1,23 @@
 import { createSuite, createRegistry } from './helper/helper'
 import { Runner } from '../lib/index'
+import { getLogAdapter } from '../lib/index'
 
-test('Run normal without any errors', () => {
-  debugger
-  const registry = createRegistry()
-  const suiteDefiniton = createSuite()
-  const runner = new Runner({ stepRegistry: registry })
-  runner.run(suiteDefiniton)
-})
+const logAdapter = getLogAdapter()
+const TIMEOUT = 30000
+
+test(
+  'Run normal without any errors',
+  async done => {
+    const registry = createRegistry()
+    const suiteDefiniton = createSuite()
+    const runner = new Runner({
+      stepRegistry: registry,
+      logAdapter,
+      parallelExecution: true,
+    })
+
+    await runner.run(suiteDefiniton)
+    done()
+  },
+  TIMEOUT
+)
