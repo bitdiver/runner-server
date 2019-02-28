8202a5ce0a7bf6b4f46989169921073915bf4bfd
Torsten Link <torstenlink@gmx.de>
2019-02-28 22:52:52 +0100

    feat(ProgressBar): add a new type of Progress bar

---
+++ src/ProgressBarConsoleLog.js
@@ -0,0 +1,51 @@
+import ProgressMeter from './ProgressMeter'
+
+export default class ProgressBarConsoleLog extends ProgressMeter {
+  _printHeader() {
+    console.log(`------------------------------------------------`) // eslint-disable-line no-console
+    console.log(`| Execute suite:           ${this.name}`) // eslint-disable-line no-console
+    console.log(`| Total step count:        ${this.stepCount}`) // eslint-disable-line no-console
+    console.log(`| Total test case count:   ${this.testcaseCount}`) // eslint-disable-line no-console
+    console.log(`------------------------------------------------`) // eslint-disable-line no-console
+  }
+
+  _printFooter() {
+    console.log(`------------------------------------------------`) // eslint-disable-line no-console
+    console.log(`| Result for suite:        ${this.name}`) // eslint-disable-line no-console
+    // eslint-disable-next-line no-console
+    console.log(
+      `| Steps:                   ${this.currentStep}/${this.stepCount}`
+    )
+    // eslint-disable-next-line no-console
+    console.log(
+      `| Testcase:                ${this.currentTestcase}/${this.testcaseCount}`
+    )
+    // eslint-disable-next-line no-console
+    console.log(`| Failed:                  ${this.testcaseFailed}`)
+    console.log(`| Last step:               ${this.lastStep}`) // eslint-disable-line no-console
+    console.log(`| Last test case:          ${this.lastTestcase}`) // eslint-disable-line no-console
+    console.log(`------------------------------------------------`) // eslint-disable-line no-console
+  }
+
+  done() {
+    super.done()
+    this._printFooter()
+  }
+
+  init(opts) {
+    super.init(opts)
+    this._printHeader()
+  }
+
+  /**
+   * Increments the current step count. Will be called when starting
+   * a new step.
+   * @param name {string} The name of the current step
+   */
+  incStep(name) {
+    super.incStep(name)
+
+    // eslint-disable-next-line no-console
+    console.log(`${this.currentStep}/${this.stepCount} ${this.lastStep}`)
+  }
+}
